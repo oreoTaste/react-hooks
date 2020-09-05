8b3ce7057c0d91395f2544aabcbd96fdfacbc213
Youngkuk Sohn <youngkuk.sohn@gmail.com>
2020-09-05 13:39:19 +0900

    1.3 useTabs 복습

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-const item = [
+const contents = [
   {
     index: "Section1",
     content: "This is the Content of Section 1",
@@ -11,13 +11,34 @@ const item = [
   },
 ];
 
-const useTabs = (initialTab, allTabs) => {
-  const [item, setItem] = useState(initialTab);
-  return allTabs.initialTab;
+const useTabs = (initalTab, allTabs) => {
+  const [currentIndex, setCurrentIndex] = useState(initalTab);
+  if (!allTabs || !Array.isArray(allTabs)) {
+    return;
+  }
+  return {
+    currentItem: allTabs[currentIndex],
+    changeItem: setCurrentIndex,
+  };
 };
 
 const App = () => {
-  return <div className="App"></div>;
+  const { currentItem, changeItem } = useTabs(0, contents);
+  return (
+    <>
+      {contents.map((content, index) => (
+        <button
+          onClick={() => {
+            changeItem(index);
+          }}
+        >
+          {content.index}
+        </button>
+      ))}
+
+      <div>{currentItem.content}</div>
+    </>
+  );
 };
 
 export default App;
