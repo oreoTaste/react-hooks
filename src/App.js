b59178b863de95507c0dd8131f8f13b17325ac4f
Youngkuk Sohn <youngkuk.sohn@gmail.com>
2020-09-05 16:20:36 +0900

    2.0 Introduction to useEffect

--- src/App.js
+++ src/App.js
@@ -1,42 +1,31 @@
-import React, { useState } from "react";
-
-const contents = [
-  {
-    index: "Section1",
-    content: "This is the Content of Section 1",
-  },
-  {
-    index: "Section2",
-    content: "This is the Content of Section 2",
-  },
-];
-
-const useTabs = (initalTab, allTabs) => {
-  const [currentIndex, setCurrentIndex] = useState(initalTab);
-  if (!allTabs || !Array.isArray(allTabs)) {
-    return;
-  }
-  return {
-    currentItem: allTabs[currentIndex],
-    changeItem: setCurrentIndex,
-  };
-};
+import React, { useEffect, useState } from "react";
 
 const App = () => {
-  const { currentItem, changeItem } = useTabs(0, contents);
+  const DidMount = () => {
+    console.log("Component Did Mount!");
+  };
+  useEffect(DidMount, []);
+
+  const [num1, setNum1] = useState(0);
+  const [num2, setNum2] = useState(0);
+
   return (
     <>
-      {contents.map((content, index) => (
-        <button
-          onClick={() => {
-            changeItem(index);
-          }}
-        >
-          {content.index}
-        </button>
-      ))}
-
-      <div>{currentItem.content}</div>
+      <div>Hello</div>
+      <button
+        onClick={() => {
+          setNum1(num1 + 1);
+        }}
+      >
+        {num1}
+      </button>
+      <button
+        onClick={() => {
+          setNum2(num2 + 1);
+        }}
+      >
+        {num2}
+      </button>
     </>
   );
 };
