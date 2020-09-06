cef0814c715d12d2c33a9d6e9d0179b5e185621b
Youngkuk Sohn <youngkuk.sohn@gmail.com>
2020-09-06 15:59:29 +0900

    2.3 useConfirm & usePreventLeave 복습

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import React from "react";
 const useConfirm = (msg = "", callback, reject) => {
   const myconfirm = () => {
     if (window.confirm(msg)) {
-      if (typeof callback === "function") callback();
+      if (typeof callback === "function") {
+        callback();
+      }
     } else {
-      if (typeof reject === "function") reject();
+      if (typeof reject === "function") {
+        reject();
+      }
     }
   };
   return myconfirm;
@@ -16,22 +20,25 @@ const usePreventLeave = () => {
     e.preventDefault();
     e.returnValue = "";
   };
-  const enablePrevent = () => window.addEventListener("beforeunload", listener);
-  const disablePrevent = () => window.removeEventListener("beforeunload", listener);
-
-  return { enablePrevent, disablePrevent };
+  const enableProtect = () => window.addEventListener("beforeunload", listener);
+  const disableProtect = () => window.removeEventListener("beforeunload", listener);
+  return { enableProtect, disableProtect };
 };
 
 const App = () => {
-  const { enablePrevent, disablePrevent } = usePreventLeave();
-  const callback = () => console.log("confirmed");
-  const reject = () => console.log("rejected");
-  const deleteWorld = useConfirm("are you sure?", callback, reject);
+  const opt1 = () => (document.querySelector(".mychoice").innerHTML = "You chose to DELETE world");
+  const opt2 = () => (document.querySelector(".mychoice").innerHTML = "You chose to PROTECT world");
+  const check = useConfirm("Are you sure?", opt1, opt2);
+
+  const { enableProtect, disableProtect } = usePreventLeave();
   return (
     <>
-      <button onClick={enablePrevent}>Protect!</button>
-      <button onClick={disablePrevent}>UnProtect!</button>
-      <button onClick={deleteWorld}>check!</button>
+      <button onClick={check}>Delete world!</button>
+      <p>
+        <button onClick={enableProtect}>Protect from sudden close</button>
+        <button onClick={disableProtect}>UnProtect from sudden close</button>
+      </p>
+      <div className="mychoice"></div>
     </>
   );
 };
