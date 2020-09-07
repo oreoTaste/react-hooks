ec387cdea475478c510f4c17fafc75554d67853e
Youngkuk Sohn <youngkuk.sohn@gmail.com>
2020-09-07 15:19:49 +0900

    useFadeIn 복습

---
+++ src/useFadeIn.js
@@ -0,0 +1,18 @@
+import { useRef, useEffect } from "react";
+
+const useFadeIn = (time = 0, delay = 0) => {
+  const el = useRef();
+  useEffect(() => {
+    var fade_str = "";
+    if (typeof time !== "number") fade_str += `all 0s`;
+    else fade_str += `all ${time}s`;
+    if (typeof delay !== "number") fade_str += ` cubic-bezier(0,0,1,1) 0s`;
+    else fade_str += ` cubic-bezier(0,0,1,1) ${delay}s`;
+    el.current.style.opacity = 1;
+    el.current.style.transition = fade_str;
+  }, []);
+
+  return { ref: el, style: { opacity: 0 } };
+};
+
+export default useFadeIn;
